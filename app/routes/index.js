56058676a0bf84067011b7259cd745c752fcb10b
Kartikeya <kartikeya107@gmail.com>
2016-06-21 17:12:42 +0000

    Added support to know latest search queries

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,34 @@
-module.exports = function(app) {
+module.exports = function(app, db) {
     
     app.route("/api/imagesearch/:searchQuery").get(function(req, res) {
         var googleImages = require("google-images");
         var client = googleImages("000207175297153202558:1qwizkolefk", "AIzaSyAnX5qH5CS-kbaoS6J-e39ZwqRAaUYG5hk");
         var searchQuery = req.params.searchQuery;
-        //var offset = req.query.offset;
-        console.log(req.query);
+        var offset = req.query.offset;
+        var searchCollection = db.collection("imagesearch");
+        console.log(offset);
+	if(offset==undefined) {
         client.search(searchQuery).then(function (images) {
+	    searchCollection.insert({"term": searchQuery, "when": new Date()});
             res.send(JSON.stringify(images));
         });
+	}
+	else {
+	client.search(searchQuery, {page: parseInt(offset)}).then(function (images) {
+	    searchCollection.insert({"term": searchQuery, "when": new Date()});
+            res.send(JSON.stringify(images));
+        });
+
+	}
     });
+    
+    app.route("/api/latest/imagesearch/").get(function(req, res) {
+        var searchCollection = db.collection("imagesearch");
+        searchCollection.find({}, {"_id":false}).toArray(function(err, doc) {
+            if(err) {
+                throw new Error(err);
+            }
+            res.send(JSON.stringify(doc));
+        })
+    })
 };
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 'use strict';
-
+var mongo = require("mongodb");
+var mongoClient = mongo.MongoClient;
 var express = require('express');
 var routes = require('./app/routes/index.js');
 var app = express();
 
+mongoClient.connect("mongodb://localhost:27017/imagesearch", function(err, db) {
+        if(err) throw err;
         app.use("/public", express.static(process.cwd()+"/public"));
 
-        routes(app);
+        routes(app,db);
         
 //	var port = server.listen(process.env.PORT || 3000);
         app.listen(8080, function(){
         console.log("listening on port "+ process.env.PORT);
         });
 
-
+});
 
